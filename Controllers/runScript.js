5f6a68f0e465bc347ae485e5e4953bfa86fba7be
Sidhant <sidhantbahri01@gmail.com>
2023-01-23 12:20:32 +0530

    backend final

--- Controllers/runScript.js
+++ Controllers/runScript.js
@@ -4,6 +4,11 @@ import uploadInDB from './uploadInDB.js';
 import path from 'path'
 
 function runScript(req, res) {
+    fs.readdirSync('./run').forEach(oldFile => {
+        fs.unlink(path.join('./run', oldFile), (err) => {
+            if (err) throw err;
+        });
+    });
     const child = spawn('python', ['scrypt.py'], { shell: true });
     var resData = ""
     child.stdout.on('data', (data) => {
@@ -26,29 +31,24 @@ function runScript(req, res) {
         // console.log(`child process exited with code ${code}`);
         // res.send(JSON.stringify(img))
         const testFolder = './run'
-        var  fiiile;
+        var fiiile;
         fs.readdirSync(testFolder).forEach(file => {
             fiiile = file
-          });
+        });
         //   console.log(fiiile)
-          const path__ = path.join(testFolder, fiiile)
-          const ext = path.extname(fiiile);
+        const path__ = path.join(testFolder, fiiile)
+        const ext = path.extname(fiiile);
         //   console.log(ext);
-           const img = {
-            data: fs.readFileSync(path__,"base64"),
+        const img = {
+            data: fs.readFileSync(path__, "base64"),
             output: resData,
             err: " ",
-            contentType : ext,
-        } 
+            contentType: ext,
+        }
         uploadInDB(img);
         // console.log(img.data);         
         console.log(`child process exited with code ${code}`);
         res.send(JSON.stringify(img));
-
-        // fs.unlink(path.join(testFolder, fiiile), (err) => {
-        //     if (err) throw err;
-        // });
-
     });
 };
 export default runScript;
